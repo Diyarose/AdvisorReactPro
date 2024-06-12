08d858677bc410db6e5138f454d1e4f51d9f2d3e
Diyarose <dhiyarosedavis@gmail.com>
2024-06-12 22:20:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddAdvisor from './components/AddAdvisor';
 import SearchUser from './components/SearchUser';
+import ViewAdvisor from './components/ViewAdvisor';
 
 function App() {
   return (
     <div>
       <AddAdvisor/>
       <SearchUser/>
+      <ViewAdvisor/>
     </div>
   );
 }
